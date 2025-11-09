185e126f792fd0ce3a60e1984cf187b5cec991ce
hisyamul <hisyamuliffat@gmail.com>
2025-11-09 21:27:32 +0700

    nilaikelompokmodif14.java

--- Jobsheet08/nilaikelompok14.java
+++ Jobsheet08/nilaikelompok14.java
@@ -5,28 +5,38 @@ public class nilaikelompok14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai,totalNilai,rataNilai;
-        totalNilai=0;
+        double nilai, totalNilai, rataNilai;
+        double rataTertinggi = 0;
+        int kelompokTertinggi = 0;
 
         int i = 1;
-while (i<=6){
-            System.out.println("kelompok" + i);
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+            totalNilai = 0;
 
+           
+            for (int j = 1; j <= 5; j++) {
+                System.out.print("Masukkan nilai ke-" + j + ": ");
+                nilai = sc.nextDouble();
+                totalNilai += nilai;
+            }
 
-        for (int j=1;j<=5;j++) {
-            System.out.println("masukkan nilai ke-" + j + ":");
-            nilai = sc.nextDouble();
-            totalNilai += nilai;
+           
+            rataNilai = totalNilai / 5;
+            System.out.println("Rata-rata: " + rataNilai);
+
+           
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+
+            i++;
+            System.out.println(); 
         }
-            rataNilai = totalNilai/5;
- System.out.println("rata-rata:" + rataNilai);
- i++;
-            
-
-}
- 
 
         
+        System.out.println("Kelompok dengan rata-rata tertinggi:");
+        System.out.println("Kelompok " + kelompokTertinggi + " dengan rata-rata " + rataTertinggi);
     }
-    
 }
