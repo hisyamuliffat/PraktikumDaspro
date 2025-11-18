87c8e2a05783a7e563f8632af355790ee240a415
hisyamul <hisyamuliffat@gmail.com>
2025-11-18 18:47:47 +0700

    modifnilaitertinggi&terendah

--- Jobsheet09/arrayratanilai14.java
+++ Jobsheet09/arrayratanilai14.java
@@ -1,43 +1,39 @@
 import java.util.Scanner;
+
 public class arrayratanilai14 {
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-       int [] nilaiMhs= new int[10];
-       double total = 0;
-       double rata2;
-      
-       int jmlmhsLulus=0;
-       int jmlmhstdkLulus=0;
-       
+        int[] nilaiMhs = new int[10];
+        double total = 0;
+        double rata2;
 
-       for (int i=0; i < nilaiMhs.length;i++){
-        System.out.println("masukkan nilai mahasiswa ke-"+(i+1)+":");
-        nilaiMhs[i]=sc.nextInt();
-        if (nilaiMhs[i] >70){
-            jmlmhsLulus+= nilaiMhs[i];
-       }if (nilaiMhs[i] <70){
-            jmlmhstdkLulus+=nilaiMhs[i];
+        double tertinggi = 0;
+        double terendah = 100;
 
-       }
+        
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMhs[i] = sc.nextInt();
 
-    }for (int i=0;i<nilaiMhs.length;i++){
-        total += nilaiMhs[i];
-       }
-       
-      
+            if (nilaiMhs[i] > tertinggi) {
+                tertinggi = nilaiMhs[i];
+            }
+
+            if (nilaiMhs[i] < terendah) {
+                terendah = nilaiMhs[i];
+            }
+        }
 
        
-       
-       rata2 = total/nilaiMhs.length;
-       double rata2mahasiswalulus = jmlmhsLulus/nilaiMhs.length;
-       double rata2mahasiswatidaklulus = jmlmhstdkLulus/nilaiMhs.length;
-       System.out.println("rata-rata nilai ="+rata2);
-        System.out.println("Rata Rata mahasiswa lulus=" + rata2mahasiswalulus);
-         System.out.println("Rata Rata mahasiswa tidak lulus=" + rata2mahasiswatidaklulus);
+        for (int i = 0; i < nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+        }
 
-       
+        rata2 = total / nilaiMhs.length;
 
+        System.out.println("Rata-rata nilai = " + rata2);
+        System.out.println("Nilai Terendah Mahasiswa = " + terendah);
+        System.out.println("Nilai Tertinggi Mahasiswa = " + tertinggi);
     }
-    
 }
