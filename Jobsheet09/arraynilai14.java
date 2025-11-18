2547d5198a288894eb6e705179f34fde4875c1a9
hisyamul <hisyamuliffat@gmail.com>
2025-11-18 14:28:29 +0700

    modifarraynilai14

--- Jobsheet09/arraynilai14.java
+++ Jobsheet09/arraynilai14.java
@@ -7,16 +7,25 @@ public class arraynilai14 {
 
         int [] nilaiAkhir = new int [10];
 
-        for (int i=0;i<10;i++){
+        for (int i=0;i<nilaiAkhir.length;i++){
         System.out.println("masukkan nilai akhir ke-"+i+":");
         nilaiAkhir[i] = sc.nextInt();
         }
         
         for (int i=0;i<10;i++){
             System.out.println("nilai akhir ke-"+i+"adalah"+nilaiAkhir[i]);
+            if (nilaiAkhir[i] <80) {
+                System.out.println("nilai akhir ke-"+i+"tidak lulus!");
+            }else if (nilaiAkhir[i] >=80){
+                System.out.println("nilai akhir ke-"+i+"Lulus");
+            }
+            
+            }
+
         }
     
 
     }
+
     
-}
+
