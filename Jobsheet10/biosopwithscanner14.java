ff7051a882a63838105fa403c4f72543130d9481
hisyamul <hisyamuliffat@gmail.com>
2025-11-24 20:35:31 +0700

    modif lengkap bioskop

--- Jobsheet10/biosopwithscanner14.java
+++ Jobsheet10/biosopwithscanner14.java
@@ -5,32 +5,77 @@ public class biosopwithscanner14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int baris,kolom;
-        String nama,next;
+        int baris, kolom;
+        String nama, next;
 
         String [][] penonton = new String [4][2];
+
         while (true) {
-            System.out.print("Masukkan nama:");
+            System.out.print("Masukkan nama: ");
             nama = sc.nextLine();
-            System.out.print("masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("masukkkan kolom:");
-            kolom = sc.nextInt();
-            sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            
+            while (true) {
+                System.out.print("Masukkan baris (1-4): ");
+                baris = sc.nextInt();
 
-            System.out.print("input penonton lainnya? (y/n):");
+                if (baris >= 1 && baris <= 4) {
+                    break; 
+                } else {
+                    System.out.println(" Nomor baris tidak tersedia! Silakan masukkan 1-4.");
+                }
+            }
+
+            
+            while (true) {
+                System.out.print("Masukkan kolom (1-2): ");
+                kolom = sc.nextInt();
+
+                if (kolom >= 1 && kolom <= 2) {
+                    break;  
+                } else {
+                    System.out.println(" Nomor kolom tidak tersedia! Silakan masukkan 1-2.");
+                }
+            }
+
+            sc.nextLine(); 
+
+            while (true) {
+                if (penonton[baris-1][kolom-1] != null) {
+                    System.out.println("Kursi sudah ditempati oleh: " + penonton[baris-1][kolom-1]);
+                    System.out.print("Masukkan nomor kolom lain (1-2): ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
+
+                    
+                    if (kolom < 1 || kolom > 2) {
+                        System.out.println("Nomor kolom tidak tersedia!");
+                        continue;
+                    }
+                } else {
+                    
+                    penonton[baris-1][kolom-1] = nama;
+                    System.out.println("Kursi berhasil dipesan!");
+                    break;
+                }
+            }
+    
+
+            System.out.print("Input penonton lainnya? (y/n): ");
             next = sc.nextLine();
-            if (next.equalsIgnoreCase("n")){
+            if (next.equalsIgnoreCase("n")) {
                 break;
             }
         }
 
         
-
-
         
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print((penonton[i][j] == null ? "***" : penonton[i][j]) + " ");
+            }
+            System.out.println();
+        }
     }
-    
 }
+
