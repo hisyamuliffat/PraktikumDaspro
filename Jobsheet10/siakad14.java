09bb254ddaaadc292dc7ab1585bce54f45ca3ff6
hisyamul <hisyamuliffat@gmail.com>
2025-11-25 22:03:24 +0700

    modifikasi si akad 14

---
+++ Jobsheet10/siakad14.java
@@ -0,0 +1,46 @@
+package Jobsheet10;
+import java.util.Scanner;
+
+public class siakad14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Masukkan jumlah siswa: ");
+        int jumlahSiswa = sc.nextInt();
+
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
+
+     
+        int[][] nilai = new int[jumlahSiswa][jumlahMatkul];
+
+       
+        for (int i = 0; i < jumlahSiswa; i++) {
+            System.out.println("Input nilai mahasiswa ke-" + (i + 1));
+            double totalPerSiswa = 0;
+
+            for (int j = 0; j < jumlahMatkul; j++) {
+                System.out.print("Nilai mata kuliah " + (j + 1) + ": ");
+                nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
+            }
+
+            System.out.println("Nilai rata-rata mahasiswa: " + totalPerSiswa / jumlahMatkul);
+        }
+
+        System.out.println("\n=========================");
+        System.out.println("Rata-rata nilai setiap mata kuliah:");
+
+       
+        for (int j = 0; j < jumlahMatkul; j++) {
+            double totalPermatkul = 0;
+
+            for (int i = 0; i < jumlahSiswa; i++) {
+                totalPermatkul += nilai[i][j];
+            }
+
+            System.out.println("Mata kuliah " + (j + 1) + ": " + totalPermatkul / jumlahSiswa);
+        }
+    }
+}
