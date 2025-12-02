84bac3f7c9b817698d87cb6059656fa81e340802
hisyamul <hisyamuliffat@gmail.com>
2025-12-02 10:52:32 +0700

    kafe14

---
+++ Jobsheet12/kafe14.java
@@ -0,0 +1,19 @@
+package Jobsheet12;
+
+public class kafe14 {
+
+public static void Menu() {
+System.out.println("====== MENU RESTO KAFE===="); 
+System.out.println("1.kopi hitam - rp 15.000");
+System.out.println("2.cappucino - rp 20.000");
+System.out.println("3.latte - rp 22.000");
+System.out.println("4.teh tarik - rp 12.000");
+System.out.println("5.roti bakar - rp 10.000");
+System.out.println("6.mie goreng - rp 18.000"); 
+System.out.println("========================");
+System.out.println("Silahkan pilih menu yang anda inginkan"); 
+}
+public static void main(String[]args){
+    Menu();
+}
+}
