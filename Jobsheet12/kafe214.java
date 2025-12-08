986050badd5c34531d4c40a502fdcb6d829348e4
hisyamul <hisyamuliffat@gmail.com>
2025-12-08 20:33:27 +0700

    modif tambah input diskon

--- Jobsheet12/kafe214.java
+++ Jobsheet12/kafe214.java
@@ -1,13 +1,40 @@
 package Jobsheet12;
+import java.util.Scanner;
+
 
 public class kafe214 {
+    
+     
+   
 
     public static void Menu(String namaPelanggan, boolean isMember) {
         System.out.println("Selamat datang, " + namaPelanggan + "!");
+        Scanner sc = new Scanner(System.in);
+        String kodePromo;
+         System.out.print("masukkan kode diskon:");
+        kodePromo = sc.nextLine();
+        
+
+        if (kodePromo.equals("DISKON50")){
+            System.out.println("Anda mendapatkan diskon 50%");
+        }else if(kodePromo.equals("DISKON30")){
+            System.out.println("Anda mendapatkan diskon 30%");
+        }else{
+            System.out.println("Kode Invalid");
+        }
+
+
+        
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+
         }
+       
+
+
+         
+        
 
         System.out.println("====== MENU RESTO KAFE====");
         System.out.println("1. kopi hitam - Rp 15.000");
@@ -21,6 +48,12 @@ public class kafe214 {
     }
 
     public static void main(String[] args) {
+       
         Menu("Andi", true);
+
+         
+
+
+    
     }
 }
